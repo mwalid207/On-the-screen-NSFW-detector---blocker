833c74d01499e81c13deb341cd03315f5d9190ec
Mohamed bin Walid <128548239+mwalid207@users.noreply.github.com>
2024-09-01 04:04:16 +0300

    Exclude the reqs of the bonk tool , Becuase I will use it directly from the nsfw-proxy:helimora docker

--- shell.nix
+++ shell.nix
@@ -1,37 +1,23 @@
 { pkgs ? import <nixpkgs> {} }:
 
-let
-  rustEnv = pkgs.mkShell {
-    buildInputs = [
-      pkgs.rustc
-      pkgs.cargo
-      pkgs.makeWrapper
-      pkgs.scdoc
-    ];
-  };
-  
-  pythonEnv = pkgs.mkShell {
-    buildInputs = [
-      pkgs.python310
-      pkgs.poetry
-      pkgs.zlib
-      pkgs.glib
-      pkgs.gcc
-      pkgs.grim # screenshot taking
-      pkgs.libnotify # notify-send
-      pkgs.stdenv.cc.cc
-    ];
-    LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
-  };
-in
-
 pkgs.mkShell {
-  buildInputs = rustEnv.buildInputs ++ pythonEnv.buildInputs;
-
-  LD_LIBRARY_PATH = pythonEnv.LD_LIBRARY_PATH;
+  buildInputs = [
+    pkgs.python310
+    pkgs.poetry
+    pkgs.zlib
+    pkgs.glib
+    pkgs.gcc
+    pkgs.grim # screenshot taking
+    pkgs.libnotify # notify-send
+    pkgs.stdenv.cc.cc
+  ];
 
   shellHook = ''
     echo 'You are now in the >>> Haram Blocker \ for Linux development environment.'
+    
+    
+    # Set Poetry to use Python 3.10
+    poetry env use python3.10
 
     # Activate the Poetry virtual environment
     poetry shell
