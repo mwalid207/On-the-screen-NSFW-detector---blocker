0320c6043471b9e9590b5fd766ac603665f15d46
Mohamed bin Walid <128548239+mwalid207@users.noreply.github.com>
2024-09-01 03:36:12 +0300

    Adding the build reqs for the Bonk cli tool

--- shell.nix
+++ shell.nix
@@ -1,23 +1,41 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  rustEnv = pkgs.mkShell {
+    buildInputs = [
+      pkgs.rustc
+      pkgs.cargo
+      pkgs.makeWrapper
+      pkgs.scdoc
+    ];
+  };
+  
+  pythonEnv = pkgs.mkShell {
+    buildInputs = [
+      pkgs.python310
+      pkgs.poetry
+      pkgs.zlib
+      pkgs.glib
+      pkgs.gcc
+      pkgs.grim # screenshot taking
+      pkgs.libnotify # notify-send
+      pkgs.stdenv.cc.cc
+    ];
+    LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+  };
+in
+
 pkgs.mkShell {
-  buildInputs = [
-    pkgs.python310
-    pkgs.poetry
-    pkgs.zlib
-    pkgs.glib
-    pkgs.gcc
-    pkgs.grim # screenshot taking
-    pkgs.libnotify # notify-send
-    pkgs.stdenv.cc.cc
-  ];
-  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+  buildInputs = rustEnv.buildInputs ++ pythonEnv.buildInputs;
+
+  LD_LIBRARY_PATH = pythonEnv.LD_LIBRARY_PATH;
+
   shellHook = ''
     echo 'You are now in the >>> Haram Blocker \ for Linux development environment.'
 
+    # Activate the Poetry virtual environment
+    poetry shell
 
-    source "$VENV_DIR/bin/activate"
-    
     # Set environment variables for grim to access the user's display
     export WAYLAND_DISPLAY=/run/user/1000/wayland-1
     export XDG_RUNTIME_DIR=/run/user/1000
